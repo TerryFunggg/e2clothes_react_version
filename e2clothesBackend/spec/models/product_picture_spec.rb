dc7f6c5cbe3974e666cc951ac0e2b0f41d6f29c1
TerryFunggg <terryyessfung@gmail.com>
2021-04-02 12:19:31 +0800

    test: ProductPircture

--- e2clothesBackend/spec/models/product_picture_spec.rb
+++ e2clothesBackend/spec/models/product_picture_spec.rb
@@ -20,5 +20,89 @@
 require 'rails_helper'
 
 RSpec.describe ProductPicture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def create_shop(title: 'test title')
+    Shop.create(
+      user: create(:user),
+      address: create(:address),
+      title: title,
+      state: Shop::UNQUALIFIED
+    )
+  end
+
+  def create_product(name: 'test')
+    Product.create(
+      name: name,
+      price: '32.0',
+      quality: 2,
+      shop: create_shop
+    )
+  end
+
+  def create_picture
+    ProductPicture.create(
+      img: 'test.jpg',
+      index: 0,
+      product: create_product
+    )
+  end
+
+  describe '#valid' do
+    it 'is valid when ProductPicture object created' do
+      p = create_picture
+      expect(p).to be_valid
+    end
+
+    it 'should past img url format correctly' do
+      p = create_picture
+      p.img = ''
+      expect(p).to be_valid
+
+      p.img = 'abc'
+      expect(p).not_to be_valid
+
+      p.img = 'jpp'
+      expect(p).not_to be_valid
+
+      %w[jpg png gif].each do |format|
+        p.img = "abc.#{format}"
+        expect(p).to be_valid
+      end
+    end
+
+    it 'is invalid when price is not a integer' do
+      p = create_picture
+      p.index = 'abc'
+      expect(p).to be_invalid
+
+      p.index = '123abc'
+      expect(p).to be_invalid
+
+      p.index = '123.abc'
+      expect(p).to be_invalid
+
+      # only integer
+      p.index = '123.321'
+      expect(p).to be_invalid
+
+      p.index = '10'
+      expect(p).to be_valid
+    end
+  end
+
+  describe '#save' do
+    it 'belongs to a product' do
+      p = ProductPicture.new(
+        img: 'test.jpg',
+        index: 0
+      )
+
+      p.save
+      expect(p).not_to be_persisted
+
+      product = create_product
+      p.product = product
+      p.save
+      expect(p).to be_persisted
+    end
+  end
 end
