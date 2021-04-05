b01bc8ed8911c757c255a969145e5ac6f1e9fe10
TerryFunggg <terryyessfung@gmail.com>
2021-04-05 11:30:23 +0800

    test model/rate

--- e2clothesBackend/spec/models/rate_spec.rb
+++ e2clothesBackend/spec/models/rate_spec.rb
@@ -22,5 +22,51 @@
 require 'rails_helper'
 
 RSpec.describe Rate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'is valid when rate created' do
+      rate = create(:rate)
+      expect(rate).to be_valid
+    end
+
+    it 'is invalid when rate is empty' do
+      r = create(:rate)
+      r.rate = ''
+      expect(r).to be_invalid
+    end
+
+    it 'is invalid when rate is not a integer' do
+      r = create(:rate)
+      r.rate = 'abc'
+      expect(r).to be_invalid
+      r.rate = 'abc.123'
+      expect(r).to be_invalid
+      r.rate = '10.2'
+      expect(r).to be_invalid
+      r.rate = '0.1'
+      expect(r).to be_invalid
+      r.rate = '4'
+      expect(r).to be_valid
+    end
+
+    it 'is invalid when rate more than 5' do
+      r = create(:rate)
+      r.rate = '6'
+      expect(r).to be_invalid
+    end
+  end
+
+  describe '#save' do
+    it 'belongs to a user and product' do
+      r = Rate.new(rate: 1)
+      r.save
+      expect(r).not_to be_persisted
+
+      user = create(:user)
+      product = create(:product)
+      r.user = user
+      r.product = product
+      r.save
+      expect(r).to be_persisted
+    end
+  end
 end
