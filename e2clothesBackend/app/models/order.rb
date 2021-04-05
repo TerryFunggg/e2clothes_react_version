95cebdecabe7a41daf79530aa7eaa8fc88522439
TerryFunggg <terryyessfung@gmail.com>
2021-04-05 13:48:51 +0800

    feat model/order add state, validation

--- e2clothesBackend/app/models/order.rb
+++ e2clothesBackend/app/models/order.rb
@@ -7,17 +7,31 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  order_log_id :bigint           not null
-#  user_id      :bigint           not null
 #
 # Indexes
 #
-#  fk_rails_f868b47f6a           (user_id)
 #  index_orders_on_order_log_id  (order_log_id) UNIQUE
 #
 # Foreign Keys
 #
 #  fk_rails_...  (order_log_id => order_logs.id)
-#  fk_rails_...  (user_id => users.id)
 #
 class Order < ApplicationRecord
+  belongs_to :order_log
+
+  STATE = [
+    COMPLETE = 'complete'.freeze,
+    CANCELED = 'canceled'.freeze,
+    FAILED = 'failed'.freeze,
+    PROCESSING = 'processing'.freeze
+  ].freeze
+
+  enum state: {
+    complete: COMPLETE,
+    canceled: CANCELED,
+    failed: FAILED,
+    processing: PROCESSING
+  }
+
+  validates :state, presence: true
 end
