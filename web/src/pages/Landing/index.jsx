01b1294038c695eb6cb32b81d8421a9065c473a9
TerryFunggg <terryyessfung@gmail.com>
2021-04-08 16:02:49 +0800

    feat page/landing create landing

---
+++ web/src/pages/Landing/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+
+const LandingPage = () => {
+
+
+  return (
+    <>
+      <h1>Hello Wrold</h1>
+    </>
+  )
+};
+
+export default LandingPage;
