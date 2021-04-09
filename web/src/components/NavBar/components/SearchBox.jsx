0f0946d42b87c244b9c48334a8c58e5031b62e4c
TerryFunggg <terryyessfung@gmail.com>
2021-04-09 14:40:21 +0800

    ui navbar/searchbox change "search" to search icon

--- web/src/components/NavBar/components/SearchBox.jsx
+++ web/src/components/NavBar/components/SearchBox.jsx
@@ -7,7 +7,9 @@ const SearchBox = () => {
         <div className="flex items-center border-b border-gray-300 py-2">
           <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search items" />
           <button className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1/2 px-2 rounded" type="button">
-            Search
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
           </button>
         </div>
       </form>
