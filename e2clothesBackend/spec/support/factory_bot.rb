ef3a6f27782d0de762443461a465fdd9c67f5d46
TerryFunggg <terryyessfung@gmail.com>
2021-04-11 01:12:59 +0800

    chore: update factory bot support json

--- e2clothesBackend/spec/support/factory_bot.rb
+++ e2clothesBackend/spec/support/factory_bot.rb
@@ -1,3 +1,17 @@
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+class JsonStrategy
+  def initialize
+    @strategy = FactoryBot.strategy_by_name(:create).new
+  end
+
+  delegate :association, to: :@strategy
+
+  def result(evaluation)
+    @strategy.result(evaluation).to_json
+  end
+end
+
+FactoryBot.register_strategy(:json, JsonStrategy)
