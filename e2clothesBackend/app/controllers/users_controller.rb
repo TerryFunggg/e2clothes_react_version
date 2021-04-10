021fd96b4cddbb6c03d28ec829708a4848167f50
TerryFunggg <terryyessfung@gmail.com>
2021-04-11 01:14:49 +0800

    feat create user controller

---
+++ e2clothesBackend/app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destory]
+
+  # GET /users
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  # POST /users
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  # GET /users/:id
+  def show
+    json_response(@user)
+  end
+
+  # PUT /users/:id
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user.is_active = false
+    @user.save
+    head :no_content
+  end
+
+  private
+
+  # white list
+  def user_params
+    # params.permit(:first_name)
+    params.permit(:email, :first_name, :last_name, :password, :phone, :role, :user_name, :is_active, :created_at,
+                  :update_at)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
