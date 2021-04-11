278a103aa7c9885126467552ec4a0b170bbc0e7f
TerryFunggg <terryyessfung@gmail.com>
2021-04-11 11:59:47 +0800

    fix contoller/user typo

--- e2clothesBackend/app/controllers/users_controller.rb
+++ e2clothesBackend/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destory]
+  before_action :set_user, only: %i[show update destroy]
 
   # GET /users
   def index
