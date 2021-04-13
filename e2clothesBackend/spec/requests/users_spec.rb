0c0f8d19c0ec3a5ffeb055773e7c7f1bd0c69cc5
TerryFunggg <terryyessfung@gmail.com>
2021-04-13 21:38:50 +0800

    test signup login

--- e2clothesBackend/spec/requests/users_spec.rb
+++ e2clothesBackend/spec/requests/users_spec.rb
@@ -101,4 +101,40 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(204)
     end
   end
+
+  describe 'POST /signup' do
+    let(:user) { build(:user) }
+    let(:headers) { valid_headers.except('Authorization') }
+    let(:valid_attributes) do
+      attributes_for(:user)
+    end
+
+    context 'when valid request' do
+      before { post '/signup', params: valid_attributes.to_json, headers: headers }
+
+      it 'create a new user' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns success message' do
+        expect(json['message']).to match(/Account created successfully/)
+      end
+
+      it 'returns an authentication token' do
+        expect(json['auth_token']).not_to be_nil
+      end
+    end
+
+    context 'when invalid request' do
+      before { post '/signup', params: {}, headers: headers }
+
+      it 'does not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns failure message' do
+        expect(json['message']).to match(/Validation failed/)
+      end
+    end
+  end
 end
