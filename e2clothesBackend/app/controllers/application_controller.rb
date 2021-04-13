c8e7570e96253f6d290a92702999e9a64f4b61a9
TerryFunggg <terryyessfung@gmail.com>
2021-04-13 22:18:10 +0800

    feat set before_action for check auth header

--- e2clothesBackend/app/controllers/application_controller.rb
+++ e2clothesBackend/app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  attr_reader :current_user
+
+  before_action :authorize_request
+
+  private
+
+  # check every request that included authorization header and token
+  # and save it to :current_user
+  def authorize_request
+    requests = AuthorizeApiRequest.new(request.headers).call
+    @current_user = requests[:user]
+  end
 end
