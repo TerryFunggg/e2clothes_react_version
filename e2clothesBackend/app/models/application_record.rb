74010a355ea473552704b917e5dd6545f30d8c7d
TerryFunggg <terryyessfung@gmail.com>
2021-04-19 13:53:59 +0800

    feat app controller pagination fn

--- e2clothesBackend/app/models/application_record.rb
+++ e2clothesBackend/app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # default per page is 10 , and start from page 1
+  # TODO: number exception
+  def self.pages(page: 1, per_page: 10)
+    p = (page.to_i / 10) + 1
+    paginate(page: p, per_page: per_page)
+  end
 end
