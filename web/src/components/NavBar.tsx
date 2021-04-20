7cb6efa5f5bd5d900984935f2ac33de445419450
TerryFunggg <terryyessfung@gmail.com>
2021-04-20 20:59:22 +0800

    style navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../hooks'
-import { activeHome, activeMarket, selectNavItemActive } from '../reducers/navItemActive'
 import { setIsLogged, selectIsLogged } from '../reducers/isLogged'
 import { gql, useQuery } from '@apollo/client'
-
 import UserDropDown from './UserDropDown'
+import { Disclosure} from '@headlessui/react'
+import { ShoppingCartIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+
 
 const ME_QUERY = gql`
 {
@@ -14,45 +15,103 @@ const ME_QUERY = gql`
         first_name
         last_name
         avatar
+        role
     }
 }
 `
+
+const navigation = [
+  { name: 'Home', href: '#', current: true },
+  { name: 'Market', href: '#', current: false },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 // TODO: responsive for user dropdown menu
 export default function NavBar() {
-    const active = useAppSelector(selectNavItemActive)
-    const isLoggedActive = useAppSelector(selectIsLogged)
-    const dispatch = useAppDispatch();
-    const { loading, error, data } = useQuery(ME_QUERY)
+  const isLoggedActive = useAppSelector(selectIsLogged)
+  const dispatch = useAppDispatch();
+  const { loading, error, data } = useQuery(ME_QUERY)
 
-    if (error) console.log(error)
+  if (error) console.log(error)
 
-    return (
-        <nav>
-            <div>
-                e2clothes
+  return (
+    <Disclosure as="nav" className="bg-white">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center text-lg">
+                  e2clothes
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-800 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button className="p-1 rounded-full text-gray-400 hover:text-gray-800 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">View Shop Cart</span>
+                  <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                {data?.me ?
+                  <UserDropDown user={data.me} />
+                  : ''
+                }
+              </div>
             </div>
+          </div>
 
-            <a style={{ color: active == 'home' ? 'red' : '' }} onClick={() => dispatch(activeHome())} href="/">Home</a>
-            <a style={{ color: active == 'market' ? 'red' : '' }} onClick={() => dispatch(activeHome())} href="/" > Market</a >
-
-            <div>
-                {data?.me ? (
-                    <UserDropDown style={{ margin: '1rem' }} user={data?.me} />
-                ) : (
-                    <>
-                        <a href="/logIn" >
-                            <button style={{ marginRight: '1rem' }}>
-                                Login
-                            </button>
-                        </a>
-                        <a href="/signUp" >
-                            <button color="blue">
-                                SignUp
-                            </button>
-                        </a>
-                    </>
-                )}
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-600 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
             </div>
-        </nav >
-    );
+          </Disclosure.Panel>
+        </>
+      )}
+
+
+    </Disclosure>
+  );
 }
--- web/src/components/UserDropDown.tsx
+++ web/src/components/UserDropDown.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React,{Fragment} from 'react'
 import { Link } from 'react-router-dom'
+import {Menu, Transition } from '@headlessui/react'
 
 
 interface UserDropDownProps {
     user: User,
-    style: React.CSSProperties
+    style?: React.CSSProperties
 }
 
 interface User {
@@ -12,19 +13,109 @@ interface User {
     first_name: string,
     last_name: string
     avatar: string
+    role: string
 }
 
+
+function classNames(...classes: string[] ) {
+    return classes.filter(Boolean).join(' ')
+  }
 
 export default function UserDropDown({ user, style }: UserDropDownProps) {
     return (
         <>
-            <div>
-                <img src={user.avatar} alt="" />
-                <p>{user.user_name}</p>
-                <ul>
-                    <a href="/profile">Your Profile</a>
-                </ul>
-            </div>
+            <Menu as="div" className="ml-3 relative">
+                  {({ open }) => (
+                    <>
+                      <div>
+                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-8 w-8 rounded-full"
+                            src={user.avatar}
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        show={open}
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items
+                          static
+                          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        >
+                             <Menu.Item>
+                              <a
+                                href="#"
+                                className='block px-4 py-2 text-sm text-gray-700'
+                              >
+                                Hi, {user.user_name}
+                              </a>
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                Your Profile
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                Your Order
+                              </a>
+                            )}
+                          </Menu.Item>
+                          { user.role === 'buyer' ? <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                Apply Shop
+                              </a>
+                            )}
+                          </Menu.Item> : ''}
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                Sign out
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </>
+                  )}
+                </Menu>
         </>
     );
 }
