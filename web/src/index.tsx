578b3b8c53f9e8bb2e90d60cfb3c48c731d123f0
TerryFunggg <terryyessfung@gmail.com>
2021-04-20 20:11:08 +0800

    feat setup tailwind css

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux'
+import './index.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
