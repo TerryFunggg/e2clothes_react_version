4e7ed59b6767b6ebb9e2bcce61358e27b4301563
TerryFunggg <terryyessfung@gmail.com>
2021-04-26 20:18:29 +0800

    style useDialog

--- web/src/components/MyDialog/useDialog.tsx
+++ web/src/components/MyDialog/useDialog.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import {DialogContent} from '../../shared/types.interface'
-/* 
+import React, { useState } from "react";
+import { DialogContent } from "../../shared/types.interface";
+/*
     Create a custom hook for Dialog
 */
 const useDialog = () => {
-    let [dialogIsActive, setDialogIsActive] = useState(false);
-    let [dialogContent, setDialogContent] = useState<DialogContent>({
-        title:'',
-        description: ''
-    });
+  let [dialogIsActive, setDialogIsActive] = useState(false);
+  let [dialogContent, setDialogContent] = useState<DialogContent>({
+    title: "",
+    description: "",
+  });
 
-    let handleDialog = (content? : DialogContent) => {
-        setDialogIsActive(!dialogIsActive);
-        if(content) setDialogContent(content)
-    }
+  let handleDialog = (content?: DialogContent) => {
+    setDialogIsActive(!dialogIsActive);
+    if (content) setDialogContent(content);
+  };
 
-    return {dialogIsActive, handleDialog, dialogContent}
+  return { dialogIsActive, handleDialog, dialogContent };
 };
 
 export default useDialog;
