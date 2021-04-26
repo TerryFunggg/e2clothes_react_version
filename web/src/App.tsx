2c99ded54d4dd07fbac1ac760b5f688236f9dc3d
TerryFunggg <terryyessfung@gmail.com>
2021-04-26 20:04:49 +0800

    fix the redirect should not use outside the router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Client from "./apollo_client";
 import Routes from "./routes";
-import IsAuthenticated from "./components/IsAuthenticated";
 import { DialogProvider } from "./components/MyDialog/MyDialogContext";
 
 function App() {
   return (
     <Client>
-      <IsAuthenticated>
-        <DialogProvider>
-          <Routes />
-        </DialogProvider>
-      </IsAuthenticated>
+      <DialogProvider>
+        <Routes />
+      </DialogProvider>
     </Client>
   );
 }
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Landing from './pages/Landing';
-import SignUp from './pages/SignUp/';
-import LogIn from './pages/Login/'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import IsAuthenticated from "./components/IsAuthenticated";
+import Landing from "./pages/Landing";
+import SignUp from "./pages/SignUp/";
+import LogIn from "./pages/Login/";
 
 function Routes() {
   return (
@@ -13,9 +14,11 @@ function Routes() {
         <Route path="/logIn">
           <LogIn />
         </Route>
-        <Route path="/">
-          <Landing />
-        </Route>
+        <IsAuthenticated>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </IsAuthenticated>
       </Switch>
     </Router>
   );
