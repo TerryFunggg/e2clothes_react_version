196d026eb63e7b9552e7b72497675ed92a3f6a41
TerryFunggg <terryyessfung@gmail.com>
2021-04-26 19:46:22 +0800

    feat add auth checking

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import Client from './apollo_client';
-import Routes from './routes'
-import {DialogProvider} from './components/MyDialog/MyDialogContext'
+import React from "react";
+import Client from "./apollo_client";
+import Routes from "./routes";
+import IsAuthenticated from "./components/IsAuthenticated";
+import { DialogProvider } from "./components/MyDialog/MyDialogContext";
 
 function App() {
   return (
     <Client>
-      <DialogProvider>
-        <Routes />
-      </DialogProvider>      
+      <IsAuthenticated>
+        <DialogProvider>
+          <Routes />
+        </DialogProvider>
+      </IsAuthenticated>
     </Client>
   );
 }
---
+++ web/src/components/IsAuthenticated.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Redirect } from "react-router";
+import { setIsLogged } from "../reducers/isLogged";
+import { useAppDispatch } from "../hooks";
+/*
+ * IsAuthenticated class use to check the user auth token
+   and will set the isLogged to ture if token is valid
+ * It will check every single times when user open the web application
+ */
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const ME_QUERY = gql`
+  {
+    me {
+      id
+      role
+    }
+  }
+`;
+
+export default function IsAuthenticated({ children }: Props) {
+  const { loading, error, data } = useQuery(ME_QUERY);
+  const dispatch = useAppDispatch();
+
+  if (error || !data?.me) return <Redirect to={{ pathname: "/" }} />;
+  else {
+    dispatch(setIsLogged(true));
+    return <>{children}</>;
+  }
+}
