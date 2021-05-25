dfc2717423c2c2c411cd710450ea6bef4beda95d
TerryFunggg <terryyessfung@gmail.com>
2021-05-25 12:06:57 +0800

    change null option

--- e2clothesBackend/app/graphql/types/MyTypes/product_type.rb
+++ e2clothesBackend/app/graphql/types/MyTypes/product_type.rb
@@ -2,16 +2,20 @@ module Types
   module MyTypes
     class ProductType < Types::BaseObject
       include Rails.application.routes.url_helpers
-      field :id, ID, null: false
-      field :shop, Types::MyTypes::ShopType, null: false
-      field :name, String, null: false
-      field :price, String, null: false
-      field :decription, String, null: true
+      field :id, ID, null: true
+      field :shop, Types::MyTypes::ShopType, null: true
+      field :name, String, null: true
+      field :price, String, null: true
+      field :description, String, null: true
       field :quality, Integer, null: true
-      field :isActive, Boolean, null: false
+      field :isActive, Boolean, null: true
       field :images, [String], null: true
-      field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-      field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+      field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+      field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+
+      def description
+        object.decription
+      end
 
       def images
         # return unless object.images.attached?
